4b836fb43871990cc3a163335a3d1ab94c3a2228
Garrison Parrish <tpgarris@live.unc.edu>
2021-09-20 17:02:47 -0400

    demo of polygon api

---
+++ polygon/rest_api_demo.py
@@ -0,0 +1,27 @@
+from polygon import RESTClient
+import datetime
+from datetime import timedelta
+
+
+def main():
+    key = "upDqU2P1_iPXrgtFpCRGG5g8fAPAX5mL"
+
+    d = datetime.date.today()
+    yesterday = d - timedelta(days=2)
+
+    # RESTClient can be used as a context manager to facilitate closing the underlying http session
+    # https://requests.readthedocs.io/en/master/user/advanced/#session-objects
+    with RESTClient(key) as client:
+        resp = client.stocks_equities_daily_open_close("AAPL", "2021-06-11")
+        print(f"On: {resp.from_} Apple opened at {resp.open} and closed at {resp.close}")
+        print(str(d))
+        print(yesterday)
+    with RESTClient(key) as client2:
+        resp2 = client2.stocks_equities_daily_open_close("AAPL", str(d))
+        print(f"On: {resp2.from_} Apple opened at {resp2.open} and closed at {resp2.close}")
+
+
+
+if __name__ == '__main__':
+    main()
+
---
+++ polygon/rest_demo_2.py
@@ -0,0 +1,41 @@
+import datetime
+
+from polygon import RESTClient
+
+
+def ts_to_datetime(ts) -> str:
+    return datetime.datetime.fromtimestamp(ts / 1000.0).strftime('%Y-%m-%d %H:%M')
+
+
+def main():
+    key = "upDqU2P1_iPXrgtFpCRGG5g8fAPAX5mL"
+    ticker: str = input("Input a ticker symbol: ")
+    start_date: str = input("Input a dash-separated start date: ")
+    end_date: str = input("Input a dash-separated end date: ")
+
+    output_path: str = f"/Users/garrisonparrish/Python Applications/stock-app/data/output:{ticker}:{start_date}:{end_date}.txt"
+
+    get_data(key, ticker, start_date, end_date, output_path)
+
+
+def get_data(key: str, ticker: str, start_date: str, end_date: str, output_path: str, interval: str = "minute"):
+    # RESTClient can be used as a context manager to facilitate closing the underlying http session
+# https://requests.readthedocs.io/en/master/user/advanced/#session-objects
+    with RESTClient(key) as client:
+        resp = client.stocks_equities_aggregates(ticker, 1, interval, start_date, end_date, unadjusted=False)
+
+        print(f"Minute aggregates for {resp.ticker} between {start_date} and {end_date}.")
+
+        with open(output_path, "a") as f:
+
+            for result in resp.results:
+                dt = ts_to_datetime(result["t"])
+                output_line: str = f"{dt}\n\tO: {result['o']}\n\tH: {result['h']}\n\tL: {result['l']}\n\tC: {result['c']} "
+                print(output_line) 
+                f.write(output_line)
+
+    f.close()
+
+
+if __name__ == '__main__':
+    main()
